d3b4cfdcd92a423ca718d211d700cd402b3b60f5
Batool-Alghamdi <batool.alghamdi@tuwaiq.edu.sa>
2021-04-19 03:32:17 +0300

    StringBuilder done

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace practice
+{
+	using System;
+	namespace Tuwaiq
+	{
+
+		public class Student
+		{
+
+			public string name;
+			public double gpa;
+			public string major;
+			public string university;
+
+			public Student(string name, double gpa, string major, string university)
+			{
+
+				this.name = name;
+				this.gpa = gpa;
+				this.major = major;
+				this.university = university;
+			}
+		}
+		class Certificate
+		{
+			private string sc;
+			public Certificate()
+			{
+				this.sc = "";
+			}
+
+			public Certificate start()
+			{
+				this.sc += "Certificate Of Completion, this award is presented to ";
+				return this;
+			}
+
+			public Certificate add(string value)
+			{
+				this.sc += value;
+				return this;
+			}
+
+			public Certificate end()
+			{
+				this.sc += "in recognition of valuable contribution";
+				return this;
+			}
+
+			public string getCert()
+			{
+				return this.sc;
+			}
+		}
+		public class Program
+		{
+			public static void Main()
+			{
+				Student student = new Student("Batool Alghamdi", 3.0, "Computer Science", "University of Colorado at Boulder");
+				string certificate = Cert(student);
+				Console.WriteLine(certificate);
+			}
+			public static string Cert(Student student)
+			{
+				Certificate sc = new Certificate();
+				string cert = sc
+					.start()
+						.add(student.name + ", with ")
+						.add("gpa  of " + student.gpa + ",")
+						.add(" in " + student.major + " major ,")
+						.add("from  "  + student.university +" " )
+					.end()
+					.getCert();
+
+				return cert;
+			}
+		}
+	}
+
+
+
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # StringBuilder
+
+<h2 align="right">المعلومات</h2>
+
+<p align="right"> المتدربة بتول الغامدي  </p>
+<p align="right">المشرفة في العتيبي</p>
+<p align="right">مجموعة البحر الأحمر</p>
