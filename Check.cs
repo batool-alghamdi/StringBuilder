0fe7c0d9bb3bb624f64cd944e2533c0dd1563c15
Batool-Alghamdi <batool.alghamdi@tuwaiq.edu.sa>
2021-04-19 05:18:51 +0300

    CheckOrder

---
+++ Check.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace practice
+{
+    public class Check
+    {
+		public static bool CheckOrder(string source)
+		{
+			Stack<char> stack = new Stack<char>();
+			stack.Push(source[0]);
+			int i = 0;
+			while(i< source.Length)
+            {
+				if (stack.Peek() == source[i])
+					stack.Pop();
+				else
+					stack.Push(source[i]);
+				i++;
+			}
+			return stack.Count == 0 ? true : false;
+		}
+		public static void Main()
+		{
+			string[] arrayOfStrings = { "12344321", "1234445353321", "1235353321", "1235335321", "893398" };
+			foreach (string str in arrayOfStrings)
+			{
+				Console.WriteLine("CheckOrder(" + str + "): " + CheckOrder(str));
+			}
+		}
+	}
+}
